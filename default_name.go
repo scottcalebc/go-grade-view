ce61bf733502947f75088efe22ea27310f7ed6c1
Caleb Scott <calebcscott@outlook.com>
2020-12-10 22:03:05 -0600

    gitflow-feature-stash: categories
    Begin adding category structs

---
+++ default_name.go
@@ -0,0 +1,8 @@
+package gradeview
+
+// Category holds information for various categories in a class such as
+// 		Assignments, Exams, Quizzes, Extra Credit, etc.
+type Category struct {
+	name   string
+	weight float32
+}
