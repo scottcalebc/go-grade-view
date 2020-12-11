a5d1596d4901a70833a335a030a17f23efb34a96
Caleb Scott <calebcscott@outlook.com>
2020-12-10 22:40:20 -0600

    Add Category
    Add category struct to be used as a structure in determining assigned points of a collection of assignments in a class. Added unit tests for default_name.go 
    !!! Will need to rename default_name.go file to standard import package -> probably gradeview.go even though that subdir creates a command file!!!!

--- cmd/gradeview/main.go
+++ cmd/gradeview/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"log"
+
+	"github.com/scottcalebc/gradeview"
 )
 
 func main() {
 	log.Println("Main started")
+
+	cat := gradeview.NewCategory("Test", 1.0)
+
+	log.Println("Created Category: ", cat)
 }
--- default_name.go
+++ default_name.go
@@ -6,3 +6,10 @@ type Category struct {
 	name   string
 	weight float32
 }
+
+// Create Category struct without accessing unexported fields
+func NewCategory(name string, weight float32) *Category {
+	p := Category{name: name, weight: weight}
+
+	return &p
+}
